b6810dc94538cf8e4adf5e829c0b443cc1078f8c
Jez Hamann <jhamann42@gmail.com>
2024-06-23 07:22:20 -0400

    v0.0.1 - Gets IDs, dls vod & chat & renders chat

--- VodManager.sh
+++ VodManager.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # VodManager
 #  Run daily via cron?
 #
@@ -10,19 +10,64 @@
 #  Gets Chapters
 #  Uploads to Youtube?
 
-vods_location = '/mnt/NAS_Drive/VODs/AlsoMij'
-channels = ['AlsoMij']
+vods_location='/mnt/NAS_Drive/VODs/AlsoMij/'
+temp_location="${vods_location}temp/"
+log_level="Status,Info,Warning,Error,Verbose"
+
+# List of users
+# https://dev.twitch.tv/docs/api/reference/#get-users
+# twitch api get /users -q login=AlsoMij
+# access them ${channels['AlsoMij']}
+declare -A channels=(["AlsoMij"]="209005581")
+
+# Configure the twitch CLI tool?
 
 # Get latest 5x vod Ids (Twitch API)
+# https://dev.twitch.tv/docs/api/reference/#get-videos
+vod_data=$(twitch api get /videos \
+  -q user_id=${channels['AlsoMij']} \
+  -q sort=time \
+  -q type=archive \
+  -q first=5
+)
 
 
-# Check if downloaded
+# Loop through all 5 video ids
+for id in $(echo ${vod_data} | jq -r '.data[].id') 
+do
 
+  # if video is alreayd downloaded
+  	# continue
+  
+  set -x
 
-# for all ids not downloaded
   # TwitchDownloadCLI vod
+  TwitchDownloaderCLI videodownload \
+    --id ${id} \
+    --log-level ${log_level} \
+    --temp-path ${temp_location} \
+    -o ${vods_location}${id}.mp4
+  
   # TwitchDownloadCLI chat
+  TwitchDownloaderCLI chatdownload -E \
+    --id ${id} \
+    --log-level ${log_level} \
+    --temp-path ${temp_location} \
+    -o ${vods_location}${id}_chat.json
+     
   # TwitchDownloadCLI render-chat
+  TwitchDownloaderCLI chatrender \
+    -i ${vods_location}${id}_chat.json \
+    -h 1080 \
+    -w 422 \
+    --framerate 30 \
+    --update-rate 0 \
+    --font-size 18 \
+    --temp-path ${temp_location} \
+    -o ${vods_location}${id}_chat.mp4
+  
   # ffmpeg combine chat ontop of vod -> new vid
+  set +x
+done
 
 # get chapters
