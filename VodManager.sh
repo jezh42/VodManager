5486034f4b117c2ac46e9af9b634b52596acc1c0
Jez <jhamann42@gmail.com>
2024-08-19 22:10:13 +1000

    v0.0.5 - minor render fix attempt

--- VodManager.sh
+++ VodManager.sh
@@ -23,6 +23,8 @@
 #  Command Line count vods
 #  Supporting multiple streamers
 #  Command line supply streamers
+#
+# Configure the twitch CLI tool?
 
 # Global Vars
 vods_location='/mnt/NAS_Drive/VODs/AlsoMij/'
@@ -30,15 +32,13 @@ temp_location="${vods_location}temp/"
 log_level="Status,Info,Warning,Error,Verbose"
 chat_height=176
 chat_width=400
-vodCount=1
+vodCount=5
 
 # List of users
 # https://dev.twitch.tv/docs/api/reference/#get-users
 # twitch api get /users -q login=AlsoMij
 # access them ${channels['AlsoMij']}
 declare -A channels=(["AlsoMij"]="209005581")
-
-# Configure the twitch CLI tool?
 
 # Get latest x vod Ids (Twitch API)
 # https://dev.twitch.tv/docs/api/reference/#get-videos
@@ -54,23 +54,32 @@ vod_data=$(twitch api get /videos \
 for id in $(echo ${vod_data} | jq -r '.data[].id') 
 do
 
-  # if video is alreayd downloaded
-  	# continue
+  # Continue if video is already downloaded
+  if [ -f "${vods_location}${id}_combined.mp4" ]; then
+    echo "Video ${id} already processed, skipping."
+    continue
+  fi
 
   echo "[VodManager] Downloading AlsoMij vod, id: ${id}"
   
   set -x
 
-  # TwitchDownloadCLI vod
+  # Download vod
   # TODO: Change the ffmpeg options to be less filesize
   TwitchDownloaderCLI videodownload \
     --id ${id} \
+    --quality Source \
     --log-level ${log_level} \
     --temp-path ${temp_location} \
     --collision Exit \
     -o ${vods_location}${id}.mp4
+
+  #if [ $? -ne 0 ]; then
+  #  echo "Error downloading video ${id}, skipping."
+  #  continue
+  #fi
   
-  # TwitchDownloadCLI chat
+  # Download chat
   TwitchDownloaderCLI chatdownload -E \
     --id ${id} \
     --log-level ${log_level} \
@@ -79,7 +88,7 @@ do
     -o ${vods_location}${id}_chat.json
 
      
-  # TwitchDownloadCLI render-chat
+  # Render chat
   # Change size
   TwitchDownloaderCLI chatrender \
     -i ${vods_location}${id}_chat.json \
@@ -95,19 +104,27 @@ do
   
   # ffmpeg combine chat ontop of vod -> new vid
   # https://stackoverflow.com/questions/52547971/overlay-transparency-video-on-top-of-other-video
-  #ffmpeg -y \
-  #  -i ${vods_location}${id}.mp4 \
-  #  -i ${vods_location}${id}_chat.mp4 \
-  #  -filter_complex \
-  #  "[1:v]format=rgb24,colorkey=black:0.3:0.2,colorchannelmixer=aa=0.3[1t]; \
-  #  [0:v][1t]overlay=W-w:0[outv]" \
-  #  -map [outv] -map 0:a \
-  #  -c:a copy \
-  #  -c:v libx264 \
-  #  -preset ultrafast \
-  #  ${vods_location}${id}_combined.mp4
+
+  #"[1:v]format=rgba,colorchannelmixer=aa=0.5[1t]; \
+  #[0:v][1t]overlay=W-w:0[outv]" \
+  # -crf 23 \
+  # -maxrate 5M -bufsize 10M \
+
+  ffmpeg -y \
+   -i ${vods_location}${id}.mp4 \
+   -i ${vods_location}${id}_chat.mp4 \
+   -filter_complex \
+   "[1:v]format=rgb24,colorkey=black:0.3:0.2,colorchannelmixer=aa=0.3[1t]; \
+   [0:v][1t]overlay=W-w:0[outv]" \
+   -map [outv] -map 0:a \
+   -c:a copy \
+   -c:v libx264 \
+   -preset slow \
+   ${vods_location}${id}_combined.mp4
   
   set +x
+
+  echo "Finished processing video ${id}"
 done
 
 # get chapters
