3379c5df0ca4b92c490c7d6cfd62091e6dc91b93
Jez Hamann <jhamann42@gmail.com>
2024-08-20 09:25:38 -0400

    v0.1.1 - better file collision and logging

--- VodManager.sh
+++ VodManager.sh
@@ -29,7 +29,8 @@
 # Global Vars
 vods_location='/mnt/NAS_Drive/VODs/AlsoMij/'
 temp_location="${vods_location}temp/"
-log_level="Status,Info,Warning,Error,Verbose"
+#log_level="Status,Info,Warning,Error,Verbose"
+log_level="Status,Info,Warning,Error"
 chat_height=176
 chat_width=400
 vodCount=5
@@ -54,57 +55,66 @@ vod_data=$(twitch api get /videos \
 for id in $(echo ${vod_data} | jq -r '.data[].id') 
 do
 
-  # Continue if video is already downloaded
-  if [ -f "${vods_location}${id}_combined.mp4" ]; then
-    echo "Video ${id} already processed, skipping."
-    continue
-  fi
+  #set -x
 
-  echo "[VodManager] Downloading AlsoMij vod, id: ${id}"
-  
-  set -x
 
-  # Download vod
-  # TODO: Change the ffmpeg options to be less filesize
-  TwitchDownloaderCLI videodownload \
+  if [ -f "${vods_location}${id}.mp4" ]; then
+    echo "[VodManager] [1] Vod ${id} already downloaded, skipping."
+  else
+    echo "[VodManager] [1] Downloading AlsoMij vod, id: ${id}..."
+    
+    # TODO: Change the ffmpeg options to be less filesize?
+    TwitchDownloaderCLI videodownload \
     --id ${id} \
     --quality Source \
     --log-level ${log_level} \
     --temp-path ${temp_location} \
-    --collision Exit \
     -o ${vods_location}${id}.mp4
 
-  #if [ $? -ne 0 ]; then
-  #  echo "Error downloading video ${id}, skipping."
-  #  continue
-  #fi
-  
-  # Download chat
-  TwitchDownloaderCLI chatdownload -E \
-    --id ${id} \
-    --log-level ${log_level} \
-    --temp-path ${temp_location} \
-    --collision Exit \
-    -o ${vods_location}${id}_chat.json
 
-     
-  # Render chat
-  # Change size
-  # --background-color "#88111111" \
-  # "#00000000"
-  #  "alternate": "#222222" "regular": "#66191919"
-  TwitchDownloaderCLI chatrender \
-    -i ${vods_location}${id}_chat.json \
-    -h ${chat_height} \
-    -w ${chat_width} \
-    --framerate 30 \
-    --update-rate 0 \
-    --font-size 18 \
-    --background-color "#00000000" \
-    --temp-path ${temp_location} \
-    --collision Overwrite \
-    --generate-mask \
-    -o ${vods_location}${id}_chat.mp4
+    #if [ $? -ne 0 ]; then
+    #  echo "Error downloading video ${id}, skipping."
+    #  continue
+    #fi
+
+  fi
+
+ 
+  if [ -f "${vods_location}${id}_chat.json" ]; then
+    echo "[VodManager] [2] Chat ${id} already downloaded, skipping."
+  else
+    echo "[VodManager] [2] Downloading AlsoMij chat, id: ${id}..."
+    
+    TwitchDownloaderCLI chatdownload -E \
+      --id ${id} \
+      --log-level ${log_level} \
+      --temp-path ${temp_location} \
+      -o ${vods_location}${id}_chat.json
+  fi 
+
+
+  
+  if [ -f "${vods_location}${id}_chat.mp4" ]; then
+    echo "[VodManager] [3] Chat ${id} already rendered, skipping."
+  else
+    echo "[VodManager] [3] Rendering AlsoMij chat, id: ${id}..."
+    # Render chat
+    # Change size
+    # --background-color "#88111111" \
+    # "#00000000"
+    #  "alternate": "#222222" "regular": "#66191919"
+    TwitchDownloaderCLI chatrender \
+      -i ${vods_location}${id}_chat.json \
+      -h ${chat_height} \
+      -w ${chat_width} \
+      --framerate 30 \
+      --update-rate 0 \
+      --font-size 18 \
+      --background-color "#00000000" \
+      --temp-path ${temp_location} \
+      --generate-mask \
+      -o ${vods_location}${id}_chat.mp4
+  fi
   
   # ffmpeg combine chat ontop of vod -> new vid
   # https://stackoverflow.com/questions/52547971/overlay-transparency-video-on-top-of-other-video
@@ -127,18 +137,31 @@ do
   #  -preset slow \
   #  ${vods_location}${id}_combined.mp4
 
-  ffmpeg -y \
-   -i ${vods_location}${id}_chat.mp4 \
-   -i ${vods_location}${id}_chat_mask.mp4 \
-   -i ${vods_location}${id}.mp4 \
-   -filter_complex "[0][1]alphamerge[ia];[2][ia]overlay=W-w:0" \
-   -c:a copy \
-   -c:v libx264 \
-   -preset slow \
-   -crf 26 \
-   ${vods_location}${id}_combined.mp4
   
-  set +x
 
-  echo "Finished processing video ${id}"
+  # Continue if video is already downloaded
+  if [ -f "${vods_location}${id}_combined.mp4" ]; then
+    echo "[VodManager] [4] Combined Video ${id} already rendered, skipping."
+  else
+    echo "[VodManager] [4] Rendering chat overlay for AlsoMij vod, id: ${id}..."
+
+    ffmpeg -y \
+     -i ${vods_location}${id}_chat.mp4 \
+     -i ${vods_location}${id}_chat_mask.mp4 \
+     -i ${vods_location}${id}.mp4 \
+     -filter_complex "[0][1]alphamerge[ia];[2][ia]overlay=W-w:0" \
+     -c:a copy \
+     -c:v libx264 \
+     -preset slow \
+     -crf 26 \
+     ${vods_location}${id}_combined.mp4
+  fi
+
+
+  #echo "[VodManager] [5] Uploading final AlsoMij vod, id: ${id}..."
+
+  echo "[VodManager] [6] Finished processing video ${id}"
+
+
+  #set +x
 done
